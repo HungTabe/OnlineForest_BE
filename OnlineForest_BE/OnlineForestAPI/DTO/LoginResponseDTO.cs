a61d1b7c03b2186ec2e72dc70a1ff32589b24470
hungtabe <trandinhhung717@gmail.com>
2025-04-17 10:02:57 +0700

    [of_14_be_v1] Develop Authen By Token In WebApp

---
+++ OnlineForest_BE/OnlineForestAPI/DTO/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineForestAPI.DTO
+{
+    public class LoginResponseDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+        public int UserId { get; set; }
+    }
+}
